8f5e4fa2e2dba3f331bd3becbf64ba8b9f9a2b41
rayjustin123 <rayjustinmendoza71@gmail.com>
2023-11-24 02:42:05 +0800

    Update database.php

--- database.php
+++ database.php
@@ -5,12 +5,12 @@ $dbname = "defaultdb";
 $username = "doadmin";
 $password = "AVNS_WDn6Taz9qQiXQ9dzgnb";
 
-// $mysqli = new mysqli(hostname: $host,
-//                      username: $username,
-//                      password: $password,
-//                      database: $dbname);
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
+
                      
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
