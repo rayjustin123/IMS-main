65cf362829235b9103056b80807972acfc8d7d49
rayjustin123 <rayjustinmendoza71@gmail.com>
2023-11-24 02:02:03 +0800

    Update database.php

--- database.php
+++ database.php
@@ -5,10 +5,12 @@ $dbname = "login_db";
 $username = "root";
 $password = "root";
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
+// $mysqli = new mysqli(hostname: $host,
+//                      username: $username,
+//                      password: $password,
+//                      database: $dbname);
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
                      
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
